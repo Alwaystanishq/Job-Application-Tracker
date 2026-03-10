8937831708db5632fe0b63fa08560c9af1084134
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-03-10 15:34:55 +0530

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ export default function Home() {
     <main className="flex-1">
     {/* Hero Section */}
     <section className="container mx-auto px-4 py-32">
-      <div>
-        <h1 className="text-black">A better way to track your job application</h1>
-        <p className="text-muted-foreground">Capture, Orgainize, and manage your job search in one place</p>
+      <div className="mx-auto max-w-4xl text-center">
+        <h1 className="text-black mb-6 text-6xl font-bold ">A better way to track your job application</h1>
+        <p className="text-muted-foreground mb-10 text-xl ">Capture, Orgainize, and manage your job search in one place</p>
+        <div className="flex flex-col items-center gap-4 ">
+          <button>Start for free</button>
+          <p>Free forever. No credit card required</p>
+        </div>
       </div>
     </section>
     </main>
