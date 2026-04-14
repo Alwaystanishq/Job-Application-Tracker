e77367e9cabfe9b7438148eae4175df80cbf92ae
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-04-14 12:06:33 +0530

    Auth Pages

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,75 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Sign In
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Enter your credentials to access your account
+          </CardDescription>
+        </CardHeader>
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                className="text-gray-700"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Password"
+                minLength={8}
+                className="text-gray-700"
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90"
+            >
+              Sign In
+            </Button>
+            <p className="text-center text-sm ext-gray-600">
+              Don&apos;t have an account{" "}
+              <Link
+                className="font-medium text-primary hover:underline"
+                href="/sign-in"
+              >
+                Sign In
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default Signin;
